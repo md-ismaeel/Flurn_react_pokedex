c3b20c8191a622dddd5586ab20c5422b5d70140c
Md Ismail <mdismaeelkhan345@gmail.com>
2024-09-02 21:50:40 +0530

    updated

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -1,7 +1,7 @@
 //endpoints
 
-export const BACKEND_END_POINT = "http://localhost:10000/api/v1/user"
-// export const BACKEND_END_POINT = "https://flurn-react-pokedex-backend.onrender.com/api/v1/user"
+// export const BACKEND_END_POINT = "http://localhost:10000/api/v1/user"
+export const BACKEND_END_POINT = "https://flurn-react-pokedex-backend.onrender.com/api/v1/user"
 
 export const requestOptions = {
     headers: {
